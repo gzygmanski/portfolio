be858770a2103dbce38f5322d12a817513f4a66c
Grzegorz Zygmański <gzygmanski@hotmail.com>
2020-07-20 17:29:12 +0200

    feat: added carousel

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
   <section id="about">
-    <div class="container">
+    <div class="container static">
       <div class="stretch secondary">
         <div class="box story-card secondary">
           <h2>2014 - Początek</h2>
@@ -38,12 +38,76 @@
         </div>
       </div>
     </div>
+    <carousel
+      :autoplay=autoplay
+      :autoplayTimeout=autoplayTimeout
+      :autoplayHoverPause=autoplayHoverPause
+      :perPage=perPage
+      :paginationEnabled=paginationEnabled
+      :loop=loop
+      :resistanceCoef=resistanceCoef
+      class="container carousel"
+    >
+      <slide class="stretch secondary">
+        <div class="box story-card secondary">
+          <h2>2014 - Początek</h2>
+          <div class="story-image">
+            <img src="../assets/sun.svg" alt=""/>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacus nisl, aliquam
+            at luctus vitae, commodo iaculis libero. Nunc vel libero et magna rhoncus aliquam sed eu
+            ante. Donec eleifend eleifend metus in rutrum. Mauris tempus magna ut dui egestas, vel
+            tincidunt ex vulputate. </p>
+        </div>
+      </slide>
+      <slide class="stretch primary">
+        <div class="box story-card primary">
+          <h2>2017 - Pierwsze projekty</h2>
+          <div class="story-image">
+            <img src="../assets/mountain.svg" alt=""/>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacus nisl, aliquam
+            at luctus vitae, commodo iaculis libero. Nunc vel libero et magna rhoncus aliquam sed eu
+            ante. Donec eleifend eleifend metus in rutrum. Mauris tempus magna ut dui egestas, vel
+            tincidunt ex vulputate. </p>
+        </div>
+      </slide>
+      <slide class="stretch tertiary">
+        <div class="box story-card tertiary">
+          <h2>2020 - Kolejne kroki</h2>
+          <div class="story-image">
+            <img src="../assets/forest.svg" alt=""/>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacus nisl, aliquam
+            at luctus vitae, commodo iaculis libero. Nunc vel libero et magna rhoncus aliquam sed eu
+            ante. Donec eleifend eleifend metus in rutrum. Mauris tempus magna ut dui egestas, vel
+            tincidunt ex vulputate. </p>
+        </div>
+      </slide>
+    </carousel>
   </section>
 </template>
 
 <script>
+import { Carousel, Slide } from 'vue-carousel'
+
 export default {
-  name: 'About'
+  name: 'About',
+  components: {
+    Carousel,
+    Slide
+  },
+  data: () => {
+    return {
+      autoplay: true,
+      autoplayTimeout: 5000,
+      autoplayHoverPause: true,
+      perPage: 1,
+      paginationEnabled: false,
+      loop: true,
+      resistanceCoef: 0
+    }
+  }
 }
 </script>
 
@@ -51,6 +115,11 @@ export default {
 @import "../less/vars.less";
 
 #about {
+
+  .carousel {
+    display: none !important;
+  }
+
   .container {
     display: flex;
     max-width: none;
@@ -65,8 +134,9 @@ export default {
         img {
           position: absolute;
           bottom: 0;
-          left: -25px;
-          max-width: 500px;
+          left: 50%;
+          transform: translateX(-50%);
+          max-width: 300px;
         }
       }
 
@@ -91,9 +161,15 @@ export default {
   }
 
   @media @sm {
-    .container {
-      flex-direction: column;
+    .static {
+      display: none;
+    }
 
+    .carousel {
+      display: block !important;
+    }
+
+    .container {
       .story-card {
         margin: auto;
         min-width: 100%;
@@ -101,9 +177,7 @@ export default {
 
         .story-image {
           img {
-            left: 50%;
-            transform: translateX(-50%);
-            max-width: 100%;
+            max-width: 50%;
           }
         }
       }
@@ -115,9 +189,15 @@ export default {
   }
 
   @media @md {
-    .container {
-      flex-direction: column;
+    .static {
+      display: none !important;
+    }
 
+    .carousel {
+      display: block !important;
+    }
+
+    .container {
       .story-card {
         margin: auto;
       }
@@ -125,6 +205,12 @@ export default {
       .story-card.secondary {
         float: none;
       }
+    }
+  }
+
+  @media @lg {
+    .carousel {
+      display: none !important;
     }
   }
 }
